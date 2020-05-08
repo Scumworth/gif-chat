d70d248ff5a40988053ffb004dcf06e64bac58fe
Daniel McNinch <danmcninch@gmail.com>
2020-05-08 00:10:07 -0700

    Edits gif box css.

--- client/src/components/GifBox.js
+++ client/src/components/GifBox.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '25px',
+    marginBottom: '25px'
+  },
+}));
 
 export default function GifBox({gif}) {
 
+  const classes = useStyles();
   return (
-    <div>
-      <iframe src={gif} width="100%" height="200px" frameborder="0"></iframe>
-    </div>
+    <Box className={classes.root}>
+      <iframe src={gif} width="50%" height="250px" frameborder="0"></iframe>
+    </Box>
   );
 }
