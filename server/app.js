79e915ee3ebdc1164fe65684bd506878830785ff
Daniel McNinch <danmcninch@gmail.com>
2020-05-08 18:09:55 -0700

    fixes typo

--- server/app.js
+++ server/app.js
@@ -74,10 +74,10 @@ io.on('connection', socket => {
 
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/../client/build')));
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
